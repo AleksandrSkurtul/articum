dcf1004c64f9658ffbb464fc1897ced31e97aa9a
Aleksandr Skurtul <alexsance@gmail.com>
2022-05-11 12:30:48 +0300

    add djoser as authentication

--- articum/settings.py
+++ articum/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'articles',
     'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+}
